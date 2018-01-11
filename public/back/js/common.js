23b3302a5f4b7cc8ca2a908f32b595b577d21859
unknown <you@example.com>
2018-01-11 23:34:28 +0800

    首页完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -8,7 +8,7 @@ $(function(){
   $(document).ajaxStop(function(){
     setTimeout(function(){
       NProgress.done();
-    },1000)
+    })
   })
 
   NProgress.configure({ showSpinner: false });
@@ -34,11 +34,53 @@ $(function(){
 
 
 
+//侧边栏二级分类功能
+
+ //给.second 的上一个li注册点击事件
+  $(".second").prev().on("click",function(){
+    //console.log("啊实打实")
+    $(this).next().slideToggle();
+  })
+
+
+//  侧边栏隐藏功能
+
+  $(".icon_menu").on("click",function(){
+    console.log("sd")
+
+    //让侧边栏隐藏
+    //主体部分padding-left:0
+    $(".lt_aside").toggleClass("now");
+    $(".lt_mian").toggleClass("now");
+  })
 
 
 
+  //退出功能
 
+  $(".icon_logout").on("click",function(){
+     $("#myModal").modal("show");
 
+    //里面会重复的注册这个点击事件，不会覆盖，导致注册很多次，
+    //1. 在外面注册
+    //2. 把之前的事件清除掉。
+  })
+
+  $(".btn_logout").on("click",function(){
+     console.log("aaaa");
+    //需要发送ajax,请求退出
+    $.ajax({
+      type:"get",
+      url:"/employee/employeeLogout",
+      success:function(info){
+        console.log(info);
+        if(info.success){
+          //跳转到登录页面
+          location.href = "login.html";
+        }
+      }
+    })
+  })
 
 
 
---
+++ public/back/js/index.js
@@ -0,0 +1,82 @@
+/**
+ * Created by Administrator on 2018/1/11.
+ */
+
+$(function(){
+  <!-- 为ECharts准备一个具备大小（宽高）的Dom -->
+
+  // 基于准备好的dom，初始化echarts实例
+  var myChart = echarts.init(document.querySelector(".chart_left"));
+
+  // 指定图表的配置项和数据
+  var option = {
+    title: {
+      text: '2017年注册人数'
+    },
+    tooltip: {},
+    legend: {
+      data:['人数']
+    },
+    xAxis: {
+      data: ["1月","2月","3月","4月","5月","6月"]
+    },
+    yAxis: {},
+    series: [{
+      name: '人数',
+      type: 'bar',
+      data: [620, 123, 300, 440, 210, 888]
+    }]
+  };
+
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+
+
+  var myChart1 = echarts.init(document.querySelector(".chart_right"));
+
+  option1 = {
+    title : {
+      text: '热门品牌虚构',
+      subtext: '2017年6月',
+      x:'center'
+    },
+    tooltip : {
+      trigger: 'item',
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left',
+      data: ['耐克','阿迪','新百伦','迪卡侬','特步']
+    },
+    series : [
+      {
+        name: '访问来源',
+        type: 'pie',
+        radius : '55%',
+        center: ['50%', '60%'],
+        data:[
+          {value:335, name:'耐克'},
+          {value:310, name:'阿迪'},
+          {value:234, name:'新百伦'},
+          {value:135, name:'迪卡侬'},
+          {value:1548, name:'特步'}
+        ],
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+
+
+  myChart1.setOption(option1);
+
+
+
+
+})
